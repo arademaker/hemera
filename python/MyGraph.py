71a21a81a3c8aef5989a32bd2d8916eadc45a9af
arademaker <arademaker@a9fab330-9d1a-0410-bf95-a02895fc6b62>
2007-04-04 13:05:07 +0000

    nova classe de grafo
    git-svn-id: http://www.tecmf.inf.puc-rio.br/svn/theorem-prover/trunk@73 a9fab330-9d1a-0410-bf95-a02895fc6b62

--- python/MyGraph.py
+++ python/MyGraph.py
@@ -1,9 +1,4 @@
-from networkx import *
 from copy import *
-
-LEFT = 'left'
-RIGHT = 'right'
-DERIVATION = 'derivation'
 
 class Node:
     def __init__(self, label):
@@ -14,38 +9,34 @@ class Node:
 	return self.label
 
     def add_child(self, node, edge):
-        assert isinstance(node, Node) and isinstance(edge, Edge)
         self.graph.add_node(node)
-        self.graph.add_edge(self, node, copy(edge))
+        self.graph.add_edge(self, node, edge)
 
     def add_childs(self, nodes, edge):
         for n in nodes:
             self.add_child(n, edge)
 
-    def get_childs(self, side=None, sign=None):
-        edges = self.get_out_edges(side=side, sign=sign)
+    def get_childs(self, func):
+        edges = self.get_out_edges(f_filter=func)
         if edges:
             return [x[0] for x in edges]
         else:
             return []
             
-    def get_out_edges(self, side=None, sign=None):
+    def get_out_edges(self, func=None):
         edges = self.graph.out_edges(self)
-        if side:
-            edges = filter(lambda x : x[2].side == side, edges)
-        if sign:
-            edges = filter(lambda x : x[2].sign == sign, edges)
+        if f_filter:
+            edges = filter(func, edges)
         if edges:
-            edges.sort(key = lambda x: x[2], cmp = lambda x,y : cmp(x.order,y.order))
-            return [(x[1], x[2]) for x in edges]
+            return edges
 	else:
 	    return []
 
     def copy_childs_from(self, node):
         A = node.get_out_edges()
-        f = lambda e: e[1].side != DERIVATION
+        f = lambda e: e[1].type != 'D'
         for (n, e) in filter(f, A):
-            self.add_child(n, copy(e))
+            self.add_child(n, e)
 
     def remove_child(self, node):
         self.graph.delete_edge(self, node)
@@ -53,10 +44,10 @@ class Node:
 
 class Edge:
     def __init__(self, type, side = None, label=None, sign=None):
-        self.type = type # Strutural, Deduction, Failure 
-        self.side = side
-        self.sign = sign
-        self.label = label
+        self.type = type   # Strutural, Deduction, Failure 
+        self.side = side   # Left, Right
+        self.sign = sign   # True, False
+        self.label = label # connector, proposition, predicate or function name...
 
     def __str__(self):
         return "%s" % (self.__dict__)
@@ -68,28 +59,30 @@ class Edge:
         self.__dict__[name] = value
 
 
-class MyGraph(XDiGraph):
+class Graph(object):
+    '''
+    The data attribute will hold a dictionary of lists:
+    { node : [ (child1, edge) , (child2, edge) , ... ] , ... }
+
+    In the original XDiGraph implementation:
+    { node : { child : edge , child : edge , ... } , ... }
+    '''
     def __init__(self):
-        XDiGraph.__init__(self)
-        self.__adjl__ = {}
+        self.data = {}
 
     def create_node(self, label):
         n = Node(label)
-        self.add_node(n)
+        self.data[n] = []
         return n
 
     def add_node(self, n):
 	try:
 	    assert isinstance(n, Node)
 	    n.graph = self
-	    XDiGraph.add_node(self, n)
+            if not self.data.has_key(n):
+                self.data[n] = []
 	except AssertionError:
 	    raise TypeError, 'Object is not a Node instance'
-
-    def append_edge(self, n1, n2):
-        if not self.__adjl__.has_key(n1):
-           self.__adjl__[n1] = []
-        self.__adjl__[n1].append(n2)
 
 
     def add_edge(self, n1, n2, edge):
@@ -97,8 +90,7 @@ class MyGraph(XDiGraph):
             assert isinstance(edge, Edge)
             self.add_node(n1)
             self.add_node(n2)
-            XDiGraph.add_edge(self, n1, n2, copy(edge))
-            self.append_edge(n1,n2)
+            self.data[n1].append( (n2,copy(edge)) )
         except AssertionError:
             raise TypeError, "Wrong type!"
 
@@ -108,23 +100,37 @@ class MyGraph(XDiGraph):
         for r in lst:
             self.add_edge(n1, r, edge)
 
+    def out_edges(self, n):
+        return self.data[n]
+
+    def delete_edge(self, n1, n2):
+        if self.data.has_key(n1):
+            if n2 in set(self.data[n1]):
+                self.data[n1].remove(n2)
+
+    def write(self):
+        for k in self.data.keys():
+            print "%s: " % k
+            for c in self.data[k]:
+                print "(%s,%s)" % (c[0],c[1]), 
+            print ""
 
 
 def teste():
-    g = MyGraph()
+    g = Graph()
     n0 = Node('|-')
     nr = Node('a')
     n1 = Node('-->')	
 
-    g.add_edges(n0, [n1], Edge('S',side=LEFT))
-    n1.add_child(nr, Edge('S', side=LEFT))
-    n1.add_child(Node('b'), Edge('S',side=RIGHT))
+    g.add_edges(n0, [n1], Edge('S',side='left'))
+    n1.add_child(nr, Edge('S',side='left'))
+    n1.add_child(Node('b'), Edge('S',side='right'))
 
     n = g.create_node('|-')
     n1.copy_childs_from(n0)
-    write_dot(g)
+    g.write()
 
     n0.remove_child(nr)
     n1.remove_child(Node('aa'))
+    g.write()
 
-    write_dot(g)
